ca2856d2f695afd969b621ff9289e4715b6d0aba
wentong <wentong@hnchasing.com>
2025-07-17 10:09:33 +0800

    feat(extension): 添加测试和生产域名
    - 未使用远程采样。如果开启远程采样，需要添加 -Dotel.service.name 参数，否则请求中不带服务名
    - 使用时需添加 -Dspring.profiles.active 参数

--- examples/extension/src/main/java/com/example/javaagent/DemoAutoConfigurationCustomizerProvider.java
+++ examples/extension/src/main/java/com/example/javaagent/DemoAutoConfigurationCustomizerProvider.java
@@ -11,8 +11,10 @@ import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvide
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 /**
  * This is one of the main entry points for Instrumentation Agent's customizations. It allows
@@ -35,40 +37,84 @@ public class DemoAutoConfigurationCustomizerProvider implements AutoConfiguratio
         ;
     }
 
+    /**
+     * 参数加载顺序：
+     * 1. getDefaultProperties
+     * 2. 命令行参数
+     * 3. customizeProperties
+     */
     private Map<String, String> customizeProperties(ConfigProperties config) {
-        String otlpEndpoint = System.getProperty("otel.exporter.otlp.endpoint");
-        if (StringUtils.isNotBlank(otlpEndpoint)) {
-            System.out.println("DemoAutoConfigurationCustomizerProvider, otel.exporter.otlp.endpoint: " + otlpEndpoint);
-            return new HashMap<>();
+        String serviceName = StringUtils.defaultIfBlank(config.getString("otel.service.name"), config.getString("spring.application.name"));
+        String activeProfile = StringUtils.defaultIfBlank(config.getString("spring.profiles.active"), System.getenv("PUBLISH_ENV"));
+        String tracesExporter = config.getString("otel.traces.exporter");
+        String tracesProtocol = config.getString("otel.exporter.otlp.traces.protocol");
+        String tracesEndpoint = config.getString("otel.exporter.otlp.traces.endpoint");
+
+        Properties systemProperties = System.getProperties();
+        if (systemProperties != null) {
+            systemProperties.forEach((k, v) -> System.out.println("DemoAutoConfigurationCustomizerProvider, customizeProperties, System.getProperties(), " + k + ": " + v));
         }
-        String activeProfile = config.getString("spring.profiles.active");
-        if (activeProfile != null && activeProfile.contains("prd")) {
-            otlpEndpoint = "http://jxlife-framework-jaeger.prd.jxlife.com.cn/v1/traces";
+        Map<String, String> systemEnv = System.getenv();
+        if (systemEnv != null) {
+            systemEnv.forEach((k, v) -> System.out.println("DemoAutoConfigurationCustomizerProvider, customizeProperties, System.getenv(), " + k + ": " + v));
+        }
+
+        if (StringUtils.isBlank(activeProfile)) {
+            // 无环境
+            System.out.println("DemoAutoConfigurationCustomizerProvider, customizeProperties, activeProfile is blank.");
+            tracesExporter = StringUtils.defaultIfBlank(tracesExporter, "none");
+        } else if (StringUtils.contains(activeProfile, "prd") || StringUtils.contains(activeProfile, "tst")) {
+            tracesExporter = StringUtils.defaultIfBlank(tracesExporter, "otlp");
+            tracesProtocol = StringUtils.defaultIfBlank(tracesProtocol, "grpc");
+            // 生产环境
+            if (StringUtils.contains(activeProfile, "prd")) {
+                tracesEndpoint = StringUtils.defaultIfBlank(tracesEndpoint, "http://jaeger-app-f5.prod.jxlife.com.cn:4317");
+                System.out.println("DemoAutoConfigurationCustomizerProvider, customizeProperties, tracesEndpoint = " + tracesEndpoint);
+            }
+            // 测试环境
+            if (StringUtils.contains(activeProfile, "tst")) {
+                tracesEndpoint = StringUtils.defaultIfBlank(tracesEndpoint, "http://jaeger-app-f5.test.jxlife.com.cn:4317");
+                System.out.println("DemoAutoConfigurationCustomizerProvider, customizeProperties, tracesEndpoint = " + tracesEndpoint);
+            }
         } else {
-            otlpEndpoint = "http://jxlife-framework-jaeger.tst.jxlife.com.cn/v1/traces";
+            // 其它环境
+            System.out.println("DemoAutoConfigurationCustomizerProvider, customizeProperties, activeProfile is neither prd or tst.");
+            tracesExporter = StringUtils.defaultIfBlank(tracesExporter, "none");
         }
+
         Map<String, String> properties = new HashMap<>();
-        properties.put("otel.exporter.otlp.endpoint", otlpEndpoint);
-        System.out.println("DemoAutoConfigurationCustomizerProvider, otel.exporter.otlp.endpoint: " + otlpEndpoint);
+        properties.put("otel.service.name", serviceName);
+        properties.put("otel.traces.exporter", tracesExporter);
+        properties.put("otel.exporter.otlp.traces.protocol", tracesProtocol);
+        properties.put("otel.exporter.otlp.traces.endpoint", tracesEndpoint);
+        System.out.println("DemoAutoConfigurationCustomizerProvider, customizeProperties, otel.exporter.traces.endpoint: " + tracesExporter + " @ " + tracesProtocol + " @ " + tracesEndpoint);
+
         return properties;
     }
 
     private Map<String, String> getDefaultProperties() {
+        System.out.println("DemoAutoConfigurationCustomizerProvider, getDefaultProperties");
         Map<String, String> properties = new HashMap<>();
+
+        // exporter
+        // properties.put("otel.traces.exporter", "none");
+        properties.put("otel.metrics.exporter", "none");
+        properties.put("otel.logs.exporter", "none");
+
         // traces
         properties.put("otel.traces.sampler", "parentbased_traceidratio");
-        properties.put("otel.traces.sampler.arg", "0.1");
+        properties.put("otel.traces.sampler.arg", "0.01");
+        // properties.put("otel.traces.sampler", "parentbased_jaeger_remote");
+        // properties.put("otel.traces.sampler.arg", "endpoint=http://10.1.143.205:5779,pollingIntervalMs=5000,initialSamplingRate=0.01");
+
+        // propagators
+        properties.put("otel.propagators", "tracecontext,baggage,jxlife");
+
         // instrumentation
         properties.put("otel.instrumentation.logback-mdc.enabled", "true");
         properties.put("otel.instrumentation.logback-mdc.add-baggage", "true");
         properties.put("otel.instrumentation.logback-appender.enabled", "true");
         properties.put("otel.instrumentation.executors.enabled", "true");
-        // propagators
-        properties.put("otel.propagators", "tracecontext,baggage,jxlife");
-        // exporter
-        properties.put("otel.traces.exporter", "otlp");
-        properties.put("otel.metrics.exporter", "none");
-        properties.put("otel.logs.exporter", "none");
 
         return properties;
     }
